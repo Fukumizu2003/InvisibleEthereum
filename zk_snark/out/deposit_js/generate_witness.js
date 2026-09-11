e381deb3d56f1e8aa323c43694468cb278d364ae
Fukumizu2003 <fukumizu2003@gmail.com>
2026-09-11 19:08:23 +0900

    zk_snark/outのLFS移行

---
+++ zk_snark/out/deposit_js/generate_witness.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4995a2e6b01432f76182abec6a831fdaf7f840384035072002ccaa43101b14d4
+size 697
---
+++ zk_snark/out/transfer_js/witness_calculator.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3bfca49ee5489996a827a1e5a0c8a1f558dbd99e657c7591f5b32e0d33189ce4
+size 10441
